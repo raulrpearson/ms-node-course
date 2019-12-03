b37d4a63729cce963479934f6f26cb8bfb975653
Raúl R. Pearson <raulrpearson@protonmail.com>
2019-12-03 16:14:53 +0100

    Complete hello-node exercise

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello from Node.js");
+const http = require("http");
+const port = 3000;
+
+const requestHandler = (req, res) => {
+  console.log(`New request to: ${req.url}`);
+  res.end("Hello, World!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));
